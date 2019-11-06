e126083e390ac043a9b140c7ef05c75b4ac067d4
Sandeep Maurya <skmaurya190@gmail.com>
2019-11-06 08:01:40 +0530

    test

--- httpserver/config.js
+++ httpserver/config.js
@@ -1,8 +1,8 @@
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://sandeep:sandeep123@127.0.0.1:27017/country',[])
-var express = require('express')
-console.log('come here')
-var app = express()
+var db = mongojs('mongodb://sandeep:sandeep123@127.0.0.1:27017/country',[]);
+var express = require('express');
+console.log('come here');
+var app = express();
 const port = 3000;
 
 
@@ -11,4 +11,4 @@ module.exports= {
     "express":express,
     "app":app,
     "port":port,
-} 
+} ;
