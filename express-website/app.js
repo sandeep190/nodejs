6b412fea3a890b918fd9122c95511a5532a8989b
sandeep190 <skmaurya190@gmail.com>
2019-11-20 21:18:54 +0530

    #express website from udemy

--- express-website/app.js
+++ express-website/app.js
@@ -5,12 +5,21 @@ var path = require('path')
 var app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false} ));
+app.use(express.static(path.join(__dirname,'public')));
 
+app.set('view engine','jade');
+app.set('views',path.join(__dirname,'views'));
 app.get('/', function(req,res){
-
-    res.send('Hello world friends');
+    data = {"title":"welcome","name":"Sandeep maurya"};
+    res.render('index',data);
+    //res.send('Hello world friends');
 });
 
+app.get('/about', function(req,res){
+    data = {"title":"welcome","name":"About Us"};
+    res.render('about',data);
+    //res.send('Hello world friends');
+});
 
 app.listen(3000);
 
