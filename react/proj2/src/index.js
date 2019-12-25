8dc3f796d6026118aa722e7846512787866d077f
sandeep190 <skmaurya190@gmail.com>
2019-12-25 06:11:50 +0530

    add react

--- react/proj2/src/index.js
+++ react/proj2/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Header(){
+   return <h1>hello ji </h1>;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Headercheck extends React.Component{
+   render(){
+      return <div><Header/>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+
+   } 
+}
+ReactDOM.render(//<Header/>,
+   <Headercheck/>,
+   document.getElementById('root') 
+    );
